9e1b14ca445d2326c363441d93ac380e7e1cd83b
ThomasS <thomass@yard.de>
2022-03-29 06:54:04 +0200

    Intro HTML page extended

--- docs/tcp-22.js
+++ docs/tcp-22.js
@@ -65,6 +65,15 @@ function addCommonArgs(args) {
     args.set("devmode",$("#chk_devMode").prop("checked"));
 }
 
+function toggleAccordion(id) {
+    var x = document.getElementById(id);
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+    } else {
+        x.className = x.className.replace(" w3-show", "");
+    }
+}
+
 function init() {
     var url = new URL(window.location.href);
     console.log("url=" + url);
